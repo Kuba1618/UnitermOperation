4f856f8f5593c0b5fdda8f781d1643e20ec02bc8
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-05-20 18:37:05 +0200

    ToogleGroup works fine

--- src/main/java/com/example/simpledrawingproject/MyController.java
+++ src/main/java/com/example/simpledrawingproject/MyController.java
@@ -6,34 +6,37 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.QuadCurveTo;
-
-import java.io.IOException;
-
 public class MyController {
 
     @FXML
-    private Pane drawingPane; // Referencja do elementu Pane na scenie
+    private Pane drawingPane;
 
     @FXML
-    public void initialize() {
+    public void onDrawingBtn(){
+        drawBezier();
+    }
+
+    @FXML
+    public void drawBezier() {
         Path path = new Path();
         path.setStroke(Color.BLACK);
         path.setStrokeWidth(2);
 
-        // Początek krzywej
-        MoveTo moveTo = new MoveTo(50, 50);
+        // Start of curve line
+        MoveTo moveTo = new MoveTo(0, 0);
 
-        // Definicja krzywej Bezier za pomocą punktów kontrolnych
+        //Continue
         QuadCurveTo quadCurveTo = new QuadCurveTo();
         quadCurveTo.setControlX(150);
         quadCurveTo.setControlY(200);
         quadCurveTo.setX(250);
         quadCurveTo.setY(50);
 
-        // Dodanie elementów do ścieżki
         path.getElements().add(moveTo);
         path.getElements().add(quadCurveTo);
 
         drawingPane.getChildren().add(path);
     }
+
+
 }
