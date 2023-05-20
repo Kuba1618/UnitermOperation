dc2b49dbb69c72081c4f1e4f64ae908a5480c3e4
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-05-20 18:21:21 +0200

    Draw Bezzier line using controller class

--- src/main/java/com/example/simpledrawingproject/MyController.java
+++ src/main/java/com/example/simpledrawingproject/MyController.java
@@ -1,41 +1,39 @@
 package com.example.simpledrawingproject;
 
-import javafx.application.Application;
-import javafx.scene.Scene;
+import javafx.fxml.FXML;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.MoveTo;
 import javafx.scene.shape.Path;
 import javafx.scene.shape.QuadCurveTo;
-import javafx.stage.Stage;
 
-public class CurveExample {
+import java.io.IOException;
 
-    public void drawDezier(Stage primaryStage) {
-        Pane root = new Pane();
+public class MyController {
 
+    @FXML
+    private Pane drawingPane; // Referencja do elementu Pane na scenie
+
+    @FXML
+    public void initialize() {
         Path path = new Path();
         path.setStroke(Color.BLACK);
         path.setStrokeWidth(2);
 
         // Początek krzywej
-        MoveTo moveTo = new MoveTo(50, 100);
+        MoveTo moveTo = new MoveTo(50, 50);
 
         // Definicja krzywej Bezier za pomocą punktów kontrolnych
         QuadCurveTo quadCurveTo = new QuadCurveTo();
         quadCurveTo.setControlX(150);
-        quadCurveTo.setControlY(50);
+        quadCurveTo.setControlY(200);
         quadCurveTo.setX(250);
-        quadCurveTo.setY(100);
+        quadCurveTo.setY(50);
 
         // Dodanie elementów do ścieżki
         path.getElements().add(moveTo);
         path.getElements().add(quadCurveTo);
 
-        root.getChildren().add(path);
-
-        Scene scene = new Scene(root, 300, 200);
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        drawingPane.getChildren().add(path);
     }
 }
