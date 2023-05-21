0dbbe7aedd4b9c660466977400a57f41a0b4eb90
Kuba1618 <jakub.prusicki1.618@gmail.com>
2023-05-21 14:00:07 +0200

    Adjust Bezier to text length works good

--- src/main/java/com/example/simpledrawingproject/MyController.java
+++ src/main/java/com/example/simpledrawingproject/MyController.java
@@ -1,6 +1,8 @@
 package com.example.simpledrawingproject;
 
 import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
@@ -14,16 +16,15 @@ import javafx.scene.shape.QuadCurveTo;
 import javafx.scene.text.*;
 public class MyController {
 
-    private int startX;
-    private int startY;
+    private int startX = 20;
+    private int startY = 30;
     private static int sizeOfFont = 14;
 
     @FXML
     private Pane drawingPane;
     @FXML
     private TextField aExapressionTxtField,bExpressionTxtField;
-    @FXML
-    private Button clearBtn;
+
     @FXML
     private RadioButton przecinekRadioBtn;
     @FXML
@@ -34,36 +35,41 @@ public class MyController {
     @FXML
     public void onClearingBtn()
     {
-        //drawingPane.getChildren().removeAll();
-        //drawingPane.setBackground(new Background(new BackgroundFill(Color.WHITE, null, null)));
         drawingPane.getChildren().clear();
+        startX = 20;
+        startY = 30;
     }
 
     @FXML
     public void onDrawingBtn(){
+        if(drawingPane.getChildren().size() > 0){
+            startY += 50;
+        }
+
         String a = aExapressionTxtField.getText();
         String b = bExpressionTxtField.getText();
         String operation = przecinekRadioBtn.isSelected()? "," : ";";
-        String expr = a + " " + operation + " " + b + " ";
+        String expr = a + " " + operation + " " + b;
 
-        startX = 50;
-        startY = 50;
-        drawBezier(startX,startY,6 * expr.length());
-
-        //printExpression() function MUST be called after calling drawBezier()
         printExpression(expr);
+        drawBezier(startX,startY, expr.length());
     }
 
     private void printExpression(String expression) {
         Text text = new Text(expression);
         text.setFont(Font.font("Arial", sizeOfFont));
         text.setFill(Color.BLACK);
-        text.setX(startY);
-        text.setY(startY + sizeOfFont + 5);
+        text.setX(startX);
+        text.setY(startY + sizeOfFont);
 
         drawingPane.getChildren().add(text);
 
-        System.out.println(expression);
+        /*Canvas canvas = new Canvas(460, 310);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        gc.setFont(new Font("Arial", 14));
+        gc.fillText(expression, startX, startY + sizeOfFont);
+        drawingPane.getChildren().add(canvas);*/
     }
 
     @FXML
@@ -75,13 +81,27 @@ public class MyController {
         // Start of curve line
         MoveTo moveTo = new MoveTo(startX,startY);
 
+        double endX = 0;//@ToDo no.1 think about adjust length of bezier line to the text length
+
+        System.out.println(length);
+        if(length <= 10){
+            endX = startX + (6 * length) + 2;
+        }
+        if(length > 10){
+            endX = startX + 7 * length - 3;
+        }
+        if(length > 19){
+            endX = startX + (6  * (length + 5)) ;
+        }
+        if(length > 28){
+            endX = startX + (7  * length) + 20;
+        }
+        System.out.println("Length of expression: " + (endX - startX));//1 - |module in Point(x,y)| 3 - (" " + "," + " ")
         //Continue
         QuadCurveTo quadCurveTo = new QuadCurveTo();
-
-        quadCurveTo.setControlX(startX + (length / 2));
-        quadCurveTo.setControlY(startY - 10);
-
-        quadCurveTo.setX(startX + length + 10);
+        quadCurveTo.setControlX(startX + ((endX - startX) / 2));
+        quadCurveTo.setControlY(startY - (0.075 * endX));
+        quadCurveTo.setX(endX);
         quadCurveTo.setY(startY);
 
         path.getElements().add(moveTo);
@@ -90,5 +110,7 @@ public class MyController {
         drawingPane.getChildren().add(path);
     }
 
-
 }
+
+//@ToDo no.2 zrobić sekwencjonowanie
+//@ToDo no.3 podpiąć MySQL Server i zpisywać dane do bazy
